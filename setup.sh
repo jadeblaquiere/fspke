d00cb6250069e0b8024e637455e3ac7d74cfb822
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2018-01-22 04:34:48 +0000

    correct s/libecc/ecclib

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get -y install check
 sudo apt-get -y install libtasn1-6-dev libtasn1-bin
 git clone https://github.com/jadeblaquiere/ecclib.git
-cd libecc
+cd ecclib
 autoreconf --install
 ./configure --prefix=/usr
 make
